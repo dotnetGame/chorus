f55e891c3be8a354ff49b868cdf5f682ebeba1fc
JasonWang <1103870790@qq.com>
2020-02-06 00:14:49 +0800

    register serializer

--- include/chorus/serialization/serialization_manager.h
+++ include/chorus/serialization/serialization_manager.h
@@ -2,17 +2,55 @@
 #define CHORUS_SERIALIZATION_MANAGER_H_
 
 #include <string>
+#include <memory>
+#include <functional>
+#include <unordered_map>
+
+#include <chorus/serialization/serializer.h>
 
 namespace chorus
 {
+    class Uint8Serializer : public ISerializer
+    {
+        virtual std::string getName() override
+        {
+            return "std::uint8_t";
+        }
+
+        virtual SObject deepCopier(const SObject& original, ICopyContext context) override
+        {
+
+        }
+
+        virtual void serializer(SObject& untypedInput, ISerializationContext context) override
+        {
+
+        }
+
+        virtual SObject deserializer(IDeserializationContext context) override
+        {
+
+        }
+    };
+
     class SerializationManager
     {
     public:
-        void registerSerializer(const std::string& type_name, );
+        void registerSerializer(const std::string& type_name, std::shared_ptr<ISerializer> serializer)
+        {
+            auto key = std::hash<std::string>{}(type_name);
+            if (serializers_.find(key) != serializers_.end()) {
+                serializers_[key] = serializer;
+            } else {
+                serializers_[key] = serializer;
+            }
+        }
     public:
         void serializeInner();
         void deserializeInner();
         void deepcopyInner();
+    private:
+        std::unordered_map<std::uint64_t, std::shared_ptr<ISerializer>> serializers_;
     };
 }
 
--- include/chorus/serialization/serializer.h
+++ include/chorus/serialization/serializer.h
@@ -2,6 +2,7 @@
 #define CHORUS_SERIALIZER_H_
 
 #include <cstdint>
+#include <string>
 
 #include <chorus/serialization/copy_context.h>
 #include <chorus/serialization/serialization_context.h>
@@ -10,17 +11,10 @@
 
 namespace chorus
 {
-    class Serializer
+    class ISerializer
     {
-        virtual SObject deepCopier(const SObject& original, ICopyContext context)=0;
+        virtual std::string getName()=0;
 
-        virtual void serializer(SObject& untypedInput, ISerializationContext context)=0;
-
-        virtual SObject deserializer(IDeserializationContext context)=0;
-    };
-
-    class Uint8Serializer : public Serializer
-    {
         virtual SObject deepCopier(const SObject& original, ICopyContext context)=0;
 
         virtual void serializer(SObject& untypedInput, ISerializationContext context)=0;
